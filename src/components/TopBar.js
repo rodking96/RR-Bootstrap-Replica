ca8f073065146033b4004b70584702fe05edb2be
Rodrigo Reyes <rodrigoreyes96@gmail.com>
2022-03-21 21:23:02 -0400

    changed font size on navbar

---
+++ src/components/TopBar.js
@@ -0,0 +1,24 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+
+export default function TopBar() {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg" >
+                <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home">
+                    <img src="https://www.instacart.com/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#LogIn">Log In</Nav.Link>
+                        <Button variant="success">Sign Up</Button>{' '}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
